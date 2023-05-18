3aa21e15e6afea4092de488177c219c566805542
bodyquadrazidbody <133974421+bodyquadrazidbody@users.noreply.github.com>
2023-05-18 14:26:54 +0200

    Add files via upload
    my first website! *some stuff will be updated as i learn more coding in js/html*

---
+++ index.js
@@ -0,0 +1,21 @@
+// this is my first javascript code!
+console.log('fortnite'); 
+
+let name = 'garry';
+console.log(name);
+
+let interestRate = 0.3;
+interestRate = 69;
+console.log(interestRate);
+
+let age = 5;
+let bobysAgeis10 = true;
+let secondName = undefined
+let lastName = null;
+let person = {
+    name: 'garry',
+    age: 5
+}; 
+
+
+console.log(person);
